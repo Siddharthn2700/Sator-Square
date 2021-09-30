18d38c4abb7380cb53c08a05f502d334cc898b33
StarDust <sid.kakroo@gmail.com>
2021-09-30 12:50:10 +0530

    final commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+namespace Sator_Square
+{
+    class Program
+    {
+        // A R E P O
+        static void Main(string[] args)
+        {
+            Console.Write('S');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('A');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('T');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('O');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('R');
+            Console.WriteLine(' ');
+            Thread.Sleep(750);
+
+            Console.Write('A');
+            Thread.Sleep(750);
+            Console.Write(' ');
+            Console.Write('R');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('E');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('P');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('O');
+            Console.WriteLine(' ');
+
+            Console.Write('T');
+            Thread.Sleep(750);
+            Console.Write(' ');
+            Console.Write('E');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('N');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('E');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('T');
+            Console.WriteLine(' ');
+
+            Console.Write('O');
+            Thread.Sleep(750);
+            Console.Write(' ');
+            Console.Write('P');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('E');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('R');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('A');
+            Console.WriteLine(' ');
+
+            Console.Write('R');
+            Thread.Sleep(750);
+            Console.Write(' ');
+            Console.Write('O');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('T');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('A');
+            Console.Write(' ');
+            Thread.Sleep(750);
+            Console.Write('S');
+            Console.Write(' ');
+            
+        }
+    }
+}
